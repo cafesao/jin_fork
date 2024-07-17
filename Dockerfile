741f9902e6797cf858b96f5003da5381b1f046d0
cafesao <my@cafesao.net>
2024-07-17 23:41:42 +0000

    init project - python for golang

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.9.19-alpine3.20
-
-WORKDIR /jin
-
-COPY . /jin
-
-RUN pip install --no-cache-dir -r requirements.txt
-
-CMD [ "python", "main.py" ]
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2024 Gabriel Dias Dutra my@cafesao.net
+*/
+package main
+
+import "github.com/cafesao/jin_fork/cmd"
+
+func main() {
+	cmd.Execute()
+}
